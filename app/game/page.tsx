53ef84fa0150e0f7bebe5d588a00289c047f8e8a
prmaher56 <prmaher56@gmail.com>
2025-09-29 16:48:17 -0400

    fixing stuff

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -4,10 +4,13 @@ import Link from "next/link"
 export default function Game() {
   return (
     <main className="overflow-hidden flex justify-center items-center bg-black text-white p-5">
-      <div className="pb-7 p-5 w-4/5 h-4/5 bg-gray-900 rounded-lg max-h-[1200px] flex flex-col">
+      <div className="drop-shadow-[0_0_80px_rgba(161,65,248,1)] pb-7 p-5 w-4/5 h-4/5 bg-gradient-to-r from-purple-600 to-purple-400 rounded-lg max-h-[1200px] flex flex-col">
+        <div>
+          <h2 className="text-5xl font-semibold pb-5 drop-shadow-[0_0_50px_rgba(255,255,255,1)] text-black">Game Name</h2>
+        </div>
         <div className="side-by-side-container flex space-x-5 items-start flex-grow">
           {/* Image box - Left side */}
-          <div className="bg-gradient-to-r from-purple-600 to-purple-400 rounded-lg drop-shadow-[0_0_30px_rgba(0,0,0,1)] w-fit">
+          <div className="bg-gray-900 rounded-lg drop-shadow-[0_0_4px_rgba(0,0,0,1)] w-fit">
             <Image
               className="flex justify-center items-center mx-auto"
               aria-hidden
@@ -252,56 +255,59 @@ export default function Game() {
 
 
         {/* Bottom section */}
-        <div className="flex-grow flex flex-col h-[450px] space-y-4">
-          <div>
-            <h2 className="text-4xl font-semibold pt-3 pb-3 drop-shadow-[0_0_4px_rgba(0,0,0,1)] text-white">CounterStrike : Global Offensive</h2>
-            <h1 className="inline-block p-2 mt-2 text-2xl font-semibold text-white rounded-lg drop-shadow-[0_0_4px_rgba(0,0,0,1)] bg-gradient-to-r from-green-900 to-green-700">$39.99</h1>
-            <h2 className="text-sm font-semibold pt-3 pb-3 drop-shadow-[0_0_4px_rgba(0,0,0,1)] text-white">Minimum Price</h2>
-          </div>
-        
+        <div className="flex-grow flex flex-col h-[450px] space-y-4 bg-gray-900 p-5 mt-5 rounded-lg">
+          <ol className="side-by-side-container list-inside flex flex-wrap gap-3 drop-shadow-[0_0_4px_rgba(0,0,0,1)]">
+              <h1 className="text-lg">Tags</h1>
+              <li className="text-white bg-gradient-to-r from-blue-800 to-blue-600 text-black text-lg rounded-lg w-fit text-center drop-shadow-[0_0_4px_rgba(0,0,0,1)] flex items-center justify-center space-x-1 px-3">
+                FPS
+              </li>
+              <li className="text-white bg-gradient-to-r from-blue-800 to-blue-600 text-black text-lg rounded-lg w-fit text-center drop-shadow-[0_0_4px_rgba(0,0,0,1)] flex items-center justify-center space-x-1 px-1">
+                Multiplayer
+              </li>
+          </ol>
 
-
-
-        {/* TABLE LINKS*/}
+          {/* TABLE LINKS*/}
 
           <div className="bg-gray-900 rounded-lg p-4 flex-1 drop-shadow-[0_0_4px_rgba(0,0,0,1)]" style={{
             maxHeight: '150px',
             overflowY: 'auto',
             minHeight: '150px'
           }}>
-            <table  className="w-full table-fixed border-separate border-spacing-y-3">
+            
+            
+            <table className="w-full table-fixed border-separate border-spacing-y-3">
               <tr>
                 <th>Platform</th>
                 <th>Versions</th>
                 <th>Source</th>
                 <th>Price</th>
               </tr>
-              
+
               <tr className="">
                 <td className="text-center mx-auto text-white bg-gradient-to-r from-blue-800 to-blue-600 text-black text-lg rounded-lg w-fit text-center drop-shadow-[0_0_4px_rgba(0,0,0,1)] flex items-center justify-center space-x-1 px-1">
-                <Link href="https://www.google.com/" target="_blank" className="side-by-side-container flex items-center justify-center">
-                  <Image
-                    className="flex justify-center items-center pr-1"
-                    aria-hidden
-                    src="https://upload.wikimedia.org/wikipedia/commons/1/19/Windows_logo_-_2002%E2%80%932012_%28Black%29.svg"
-                    color="white"
-                    alt="File icon"
-                    width={24}
-                    height={24}
-                    style={{ filter: 'brightness(0) invert(1)' }}
-                  />
-                  Windows
-                  <Image
-                    className="flex justify-center items-center pr-1"
-                    aria-hidden
-                    src="https://www.svgrepo.com/show/510970/external-link.svg"
-                    color="white"
-                    alt="File icon"
-                    width={24}
-                    height={24}
-                    style={{ filter: 'brightness(0) invert(1)' }}
-                  />
-                </Link>
+                  <Link href="https://www.google.com/" target="_blank" className="side-by-side-container flex items-center justify-center">
+                    <Image
+                      className="flex justify-center items-center pr-1"
+                      aria-hidden
+                      src="https://upload.wikimedia.org/wikipedia/commons/1/19/Windows_logo_-_2002%E2%80%932012_%28Black%29.svg"
+                      color="white"
+                      alt="File icon"
+                      width={24}
+                      height={24}
+                      style={{ filter: 'brightness(0) invert(1)' }}
+                    />
+                    Windows
+                    <Image
+                      className="flex justify-center items-center pr-1"
+                      aria-hidden
+                      src="https://www.svgrepo.com/show/510970/external-link.svg"
+                      color="white"
+                      alt="File icon"
+                      width={24}
+                      height={24}
+                      style={{ filter: 'brightness(0) invert(1)' }}
+                    />
+                  </Link>
                 </td>
                 <td className="text-center mx-auto">
                   7,10,11
@@ -315,29 +321,29 @@ export default function Game() {
               </tr>
               <tr className="">
                 <td className="text-center mx-auto text-white bg-gradient-to-r from-blue-800 to-blue-600 text-black text-lg rounded-lg w-fit text-center drop-shadow-[0_0_4px_rgba(0,0,0,1)] flex items-center justify-center space-x-1 px-1">
-                <Link href="https://www.google.com/" target="_blank" className="side-by-side-container flex items-center justify-center">
-                  <Image
-                    className="flex justify-center items-center pr-1"
-                    aria-hidden
-                    src="https://upload.wikimedia.org/wikipedia/commons/1/19/Windows_logo_-_2002%E2%80%932012_%28Black%29.svg"
-                    color="white"
-                    alt="File icon"
-                    width={24}
-                    height={24}
-                    style={{ filter: 'brightness(0) invert(1)' }}
-                  />
-                  Windows
-                  <Image
-                    className="flex justify-center items-center pr-1"
-                    aria-hidden
-                    src="https://www.svgrepo.com/show/510970/external-link.svg"
-                    color="white"
-                    alt="File icon"
-                    width={24}
-                    height={24}
-                    style={{ filter: 'brightness(0) invert(1)' }}
-                  />
-                </Link>
+                  <Link href="https://www.google.com/" target="_blank" className="side-by-side-container flex items-center justify-center">
+                    <Image
+                      className="flex justify-center items-center pr-1"
+                      aria-hidden
+                      src="https://upload.wikimedia.org/wikipedia/commons/1/19/Windows_logo_-_2002%E2%80%932012_%28Black%29.svg"
+                      color="white"
+                      alt="File icon"
+                      width={24}
+                      height={24}
+                      style={{ filter: 'brightness(0) invert(1)' }}
+                    />
+                    Windows
+                    <Image
+                      className="flex justify-center items-center pr-1"
+                      aria-hidden
+                      src="https://www.svgrepo.com/show/510970/external-link.svg"
+                      color="white"
+                      alt="File icon"
+                      width={24}
+                      height={24}
+                      style={{ filter: 'brightness(0) invert(1)' }}
+                    />
+                  </Link>
                 </td>
                 <td className="text-center mx-auto">
                   7,10,11
@@ -352,13 +358,13 @@ export default function Game() {
             </table>
           </div>
 
-          
+
           <div className="mt-15 p-5 bg-gray-800 rounded-lg h-full overflow-y-auto">
             Comment
           </div>
         </div>
 
-        
+
       </div>
     </main>
   );
